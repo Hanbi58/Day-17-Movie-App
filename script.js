21729341052fda6db43c4e45a04dea94bc1b8d25
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-14 19:56:11 -0700

    Teacher's version. modified, to reduce Http request

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-const getMovies = async (page = 3) => {
+async function getMovies(page = 3) {
   let movieList = [];
   for (let i = 1; i <= page; i++) {
     const res = await fetch(
@@ -16,4 +16,47 @@ const getMovies = async (page = 3) => {
   }
 
   return movieList;
-};
+}
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function showMovies(pages = 5) {
+  const movieList = await getMovies(pages);
+
+  main.innerHTML = "";
+
+  movieList.forEach((movieList) => {
+    const { title, poster_path, vote_average, overview } = movieList;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+          <img src="${IMG_PATH + poster_path}" alt="${title}">
+          <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="">${vote_average}</span>
+          </div>
+          <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+      `;
+    main.appendChild(movieEl);
+  });
+}
+showMovies();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+  const movies = document.querySelectorAll(".movie");
+
+  movies.forEach((movie) => {
+    if (movie.childNodes[3].innerText.indexOf(searchTerm) === -1) {
+      main.removeChild(movie);
+    }
+  });
+});
